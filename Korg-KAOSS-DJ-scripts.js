b9e95318a9c59cb357fd6748eec6af170eed3e9d
sebdooris <sebastian.dooris@gmail.com>
2016-12-08 00:00:27 +0000

    Jog wheels

--- Korg-KAOSS-DJ-scripts.js
+++ Korg-KAOSS-DJ-scripts.js
@@ -1,15 +1,15 @@
 var KAOSSDJ = {};
 
 KAOSSDJ.wheelTouch = function (channel, control, value, status, group) {
-    if ((status & 0xF0) === 0x90) {    
+    if ((status & 0xF0) === 0x90) {    // If button down
         var alpha = 1.0/8;
         var beta = alpha/32;
-        engine.scratchEnable(1, 128, 33+1/3, alpha, beta);
-    } else {
-        engine.scratchDisable(1);
+        engine.scratchEnable(KAOSSDJ.currentDeck, 128, 33+1/3, alpha, beta);
+    } else {    // If button up
+        engine.scratchDisable(KAOSSDJ.currentDeck);
     }
 }
-
+ 
 KAOSSDJ.wheelTurn = function (channel, control, value, status, group) {
     var newValue;
     if (value < 64) {
@@ -17,9 +17,9 @@ KAOSSDJ.wheelTurn = function (channel, control, value, status, group) {
     } else {
         newValue = value - 128;
     }
-    if (engine.isScratching(1)) {
-        engine.scratchTick(1, newValue);
+    if (engine.isScratching(KAOSSDJ.currentDeck)) {
+        engine.scratchTick(KAOSSDJ.currentDeck, newValue); // Scratch!
     } else {
-        engine.setValue('[Channel'+1+']', 'jog', newValue);
+        engine.setValue('[Channel'+KAOSSDJ.currentDeck+']', 'jog', newValue); // Pitch bend
     }
 }
